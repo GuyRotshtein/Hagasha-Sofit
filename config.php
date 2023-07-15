425298fb753ce0b2a66c3d300bbcccee6dd7713a
Guy Rotshtein <100364697+GuyRotshtein@users.noreply.github.com>
2023-07-15 15:30:00 +0300

    fixed commit of config

--- config.php
+++ config.php
@@ -1,2 +1,3 @@
 <?php
-define("URL" , "http://se.shenkar.ac.il/students/2022-2023/web1/dev_95/ex5/");
+//define("URL" , "http://se.shenkar.ac.il/students/2022-2023/web1/dev_95/ex5/");
+define("URL" , "http://localhost:63342/Hagasha-Sofit/");
