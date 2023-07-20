93e06af3dfa0943c418cf7258f5a5f647ca59663
Mawasi1 <nj.mawasi@hotmail.com>
2023-07-20 14:56:36 +0300

    added admin.php page and updated userCRUD.php

--- admin.php
+++ admin.php
@@ -14,7 +14,213 @@ $result_user = mysqli_query($connection, $query_user);
 if (!$result_user) {
     die("DB query failed.");
 }
+$row_user = mysqli_fetch_assoc($result_user);
+$fName = $row_user['f_name'];
+$lName = $row_user['l_name'];
+$picture = $row_user['user_picture'];
+$gender = $row_user['gender'];
+$country = $row_user['user_country'];
+$favColor = $row_user['user_fav_color'];
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
+    <script src="./js/script.js"></script>
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <title>Clother - Home</title>
+</head>
+
+<body class="homePage">
+    <header class="p-4 py-3 border-bottom">
+        <div class="d-flex align-items-center justify-content-center justify-content-md-between ">
+            <!--    Hamburger menu-->
+            <div class="col-4">
+                <div class="mb-2 mb-md-0 header-hamburger">
+                    <button class="btn " type="button" data-bs-toggle="offcanvas" data-bs-target="#Hamburger"
+                        aria-controls="Hamburger">
+                        <img src="./images/icons/hamburger.png" height="40" width="40">
+                    </button>
+                    <div class="offcanvas offcanvas-start" tabindex="-1" id="Hamburger"
+                        aria-labelledby="HamburgerLabel">
+                        <!--        hamburger contents-->
+                        <div class="offcanvas-header">
+                            <h5 class="offcanvas-title" id="HamburgerLabel">CLOTHER</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas"
+                                aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body">
+                            <div>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item" class="nav-item"><a href="./index.php"
+                                            class="nav-link">Home</a></li>
+                                    <li class="list-group-item" class="nav-item"><a href="closetList.php"
+                                            class="nav-link">Closets</a>
+                                    </li>
+                                    <li class="list-group-item" class="nav-item"><a href="#"
+                                            class="nav-link">Calendar</a>
+                                    </li>
+                                    <li class="list-group-item" class="nav-item"><a href="#" class="nav-link">Travel</a>
+                                    </li>
+
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!--    logo      -->
+            <div class="col-4 d-flex col-md-auto mb-2 justify-content-center mb-md-0 header-logo">
+                <a class="clother-logo" href="./index.php"> <img src="./images/icons/new_logo.png"></a>
+            </div>
+            <!--    User panel    -->
+            <div class="col-4 d-flex justify-content-end text-end header-user-menu">
+                <div class="flex-shrink-0 dropdown">
+                    <button class=" btn d-block link-dark text-decoration-none dropdown-toggle" type="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
+
+                        <img src="<?php echo './uploads/user_pictures/' . $picture; ?>" alt="mdo" width="32" height="32"
+                            class="rounded-circle">
+                    </button>
+                    <ul class="dropdown-menu text-small shadow dropdown-menu-end">
+                        <li><a class="dropdown-item" href="./userSettings.php">Settings</a></li>
+                        <li><a class="dropdown-item" href="./userSettings.php">Profile</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="./logout.php">Sign out</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="row">
+            <div class="col-3 py-2 border-end border-primary-subtle border-3 desktop-menu">
+                <div class="row">
+                </div>
+                <div class="row ">
+                    <div class="col ">
+                        <nav style="--bs-breadcrumb-divider: '>';" class="px-3 py-1" aria-label="breadcrumb">
+                            <ol class="breadcrumb">
+                                <li class="breadcrumb-item " aria-current="page"><a class="breadcrumb-link"
+                                        href="./index.php">Home</a>
+                                </li>
+                                <li class="breadcrumb-item" aria-current="page"><a class="breadcrumb-link"
+                                        href="./admin.php">Admin panel</a></li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <!--        breadcrumbs         -->
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item" class="nav-item"><a href="./index.php"
+                                    class="nav-link px-3">Home</a></li>
+                            <li class="list-group-item" class="nav-item"><a href="./closetList.php"
+                                    class="nav-link px-3">Closet</a>
+                            </li>
+                            <li class="list-group-item" class="nav-item"><a href="#" class="nav-link px-3">Calendar</a>
+                            </li>
+                            <li class="list-group-item" class="nav-item"><a href="#" class="nav-link px-3">Travel</a>
+                            </li>
+                            <?php
+                            $query_admin = "SELECT is_admin FROM tbl_222_users WHERE user_id = $uid;";
+                            $result_admin = mysqli_query($connection, $query_admin);
+
+                            if (!$result_admin) {
+                                die("DB query failed.");
+                            }
+                            $row_admin = mysqli_fetch_assoc($result_admin);
+                            $admin = $row_admin['is_admin'];
+                            if ($admin) {
+                                echo '<li class="list-group-item" class="nav-item"><a href="admin.php" class="nav-link ps-3">Admin panel</a></li>';
+                            }
+                            ?>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="container-fluid ">
+                    <div class="container py-2">
+                        <div class="container main-container px-3">
+                            <div class="container text-left px-0">
+                                <div class="row">
+                                    <!--                                Search bar - use AJAX? dunno :)-->
+                                    <div class="col-3 py-1">
+                                        <h1> Admin panel</h1>
+                                    </div>
+                                    <div class="col-9 d-flex flex-row-reverse"></div>
+                                    <h5>User details</h5>
+                                </div>
+                            </div>
+
+                            <table class="table table-hover ">
+                                <thead class="table-dark">
+                                    <tr>
+                                        <th scope="col">Id</th>
+                                        <th scope="col">First name</th>
+                                        <th scope="col">Last name</th>
+                                        <th scope="col"></th>
+
+
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $query_users = "SELECT * FROM tbl_222_users WHERE is_admin = 0;";
+                                    $result_users = mysqli_query($connection, $query_users);
+
+                                    if (!$result_users) {
+                                        die("DB query failed.");
+                                    }
+
+                                    while ($row_users = mysqli_fetch_assoc($result_users)) {
+                                        $id = $row_users['user_id'];
+                                        $fName = $row_users['f_name'];
+                                        $lName = $row_users['l_name'];
+                                        $picture = $row_users['user_picture'];
+                                        $gender = $row_users['gender'];
+                                        $country = $row_users['user_country'];
+                                        $favColor = $row_users['user_fav_color'];
+
+                                        echo '<tr>';
+                                        echo '<th scope="col">' . $id . '</th>';
+                                        echo '<th scope="col">' . $fName . '</th>';
+                                        echo '<th scope="col">' . $lName . '</th>';
+                                        echo '<th scope="col">
+                                                <form name="userRemovalForm" id="userRemovalForm" action="userCRUD.php" method="POST">
+                                                    <input type="hidden" name="is_remove" value="true">
+                                                    <input type="hidden" name="del_user_id" value="' . $id . '">
+                                                    <button type="submit" class="btn text-right text-hide clothingButton removeItemBtn removeItemBtnDanger px-5 text-bg-danger">Delete User</button>
+                                                </form>
+                                              </th>';
+                                        echo '</tr>';
+                                    }
+                                    ?>
+                                </tbody>
+                            </table>
+                            <div class="row">
+                                <div class="col-6 mx-auto">
+                                    <div class=" mx-auto clothingLine d-block"></div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
--- userCRUD.php
+++ userCRUD.php
@@ -1,7 +1,7 @@
 <?php
 include "config.php";
 include "db.php";
-if (!isset($_POST['is_remove'])){
+if (!isset($_POST['is_remove'])) {
     $fName = mysqli_real_escape_string($connection, $_POST['userFName']);
     $lName = mysqli_real_escape_string($connection, $_POST['userLName']);
     $eMail = mysqli_real_escape_string($connection, $_POST['userMail']);
@@ -16,14 +16,15 @@ if (!isset($_POST['is_remove'])){
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-            crossorigin="anonymous"></script>
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossorigin="anonymous"></script>
     <script src="./js/script.js"></script>
     <link rel="stylesheet" href="./css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -31,109 +32,149 @@ if (!isset($_POST['is_remove'])){
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <title>Clother - Login</title>
 </head>
+
 <body>
-<header class="p-4 py-3 border-bottom">
-    <div class="d-flex align-items-center justify-content-center">
-        <div class="col-4 d-flex col-md-auto mb-2 justify-content-center mb-md-0 header-logo">
-            <div class="clother-logo"><img src="./images/icons/new_logo.png"></div>
+    <header class="p-4 py-3 border-bottom">
+        <div class="d-flex align-items-center justify-content-center">
+            <div class="col-4 d-flex col-md-auto mb-2 justify-content-center mb-md-0 header-logo">
+                <div class="clother-logo"><img src="./images/icons/new_logo.png"></div>
+            </div>
         </div>
-    </div>
-</header>
-<main>
-    <div class="row justify-content-center pt-3">
-        <div class="col-4 py-2">
-            <div class="container main-container rounded-4 px-3">
-                <div class="container text-left px-0">
-                    <div class="row pb-5">
-                        <div class="col-12 py-1">
-                            <h1>Welcome to Clother</h1>
-                            <div class="col mx-2"><h5>Your go-to for all clothing advice!</h5></div>
+    </header>
+    <main>
+        <div class="row justify-content-center pt-3">
+            <div class="col-4 py-2">
+                <div class="container main-container rounded-4 px-3">
+                    <div class="container text-left px-0">
+                        <div class="row pb-5">
+                            <div class="col-12 py-1">
+                                <h1>Welcome to Clother</h1>
+                                <div class="col mx-2">
+                                    <h5>Your go-to for all clothing advice!</h5>
+                                </div>
+                            </div>
+                            <div class="col-9 d-flex flex-row-reverse"></div>
                         </div>
-                        <div class="col-9 d-flex flex-row-reverse"></div>
                     </div>
-                </div>
-                <div class="row pt-2">
-                    <div class="container justify-content-center text-center rounded-5 px-5">
-                        <?php
-                        if (isset($_POST['isEdit'])){
-                            session_start();
-                            $number = $_SESSION['user_id'];
+                    <div class="row pt-2">
+                        <div class="container justify-content-center text-center rounded-5 px-5">
+                            <?php
+                            if (isset($_POST['isEdit'])) {
+                                session_start();
+                                $number = $_SESSION['user_id'];
 
-                            $fName = mysqli_real_escape_string($connection, $_POST['userFName']);
-                            $lName = mysqli_real_escape_string($connection, $_POST['userLName']);
-                            $eMail = mysqli_real_escape_string($connection, $_POST['userMail']);
-                            $pass = mysqli_real_escape_string($connection, $_POST['userPass']);
-                            $phone = mysqli_real_escape_string($connection, $_POST['userPhone']);
-                            $picture = mysqli_real_escape_string($connection, $_POST['userPicture']);
-                            $gender = mysqli_real_escape_string($connection, $_POST['userGender']);
-                            $country = mysqli_real_escape_string($connection, $_POST['userCountry']);
-                            $favColor = mysqli_real_escape_string($connection, $_POST['userColor']);
+                                $fName = mysqli_real_escape_string($connection, $_POST['userFName']);
+                                $lName = mysqli_real_escape_string($connection, $_POST['userLName']);
+                                $eMail = mysqli_real_escape_string($connection, $_POST['userMail']);
+                                $pass = mysqli_real_escape_string($connection, $_POST['userPass']);
+                                $phone = mysqli_real_escape_string($connection, $_POST['userPhone']);
+                                $picture = mysqli_real_escape_string($connection, $_POST['userPicture']);
+                                $gender = mysqli_real_escape_string($connection, $_POST['userGender']);
+                                $country = mysqli_real_escape_string($connection, $_POST['userCountry']);
+                                $favColor = mysqli_real_escape_string($connection, $_POST['userColor']);
 
-                            $query = "UPDATE tbl_222_users SET `f_name`='$fName',`l_name`='$lName',`email`='$eMail',`password`='$pass',`phone`='$phone',`user_picture`='$picture',`gender`='$gender',`user_country`='$country',`user_fav_color`='$favColor' WHERE (`user_id`='$number');";
-                            $result = mysqli_query($connection, $query);
-                            if (!$result) {
-                                die("DB update query failed.");
-                            }
-//                            sleep(3);
-                            echo 'UPDATE SUCCESFULL!';
-                            sleep(4);
-                            header('Location: ' . URL . 'userSettings.php');
-                        } else if (isset($_POST['is_remove'])){
-                            //first, get all clothes in closets that the user owns
-                            session_start();
-                            $number = $_SESSION['user_id'];
-                            $query = "DELETE clth . * FROM tbl_222_clothes clth
+                                $query = "UPDATE tbl_222_users SET `f_name`='$fName',`l_name`='$lName',`email`='$eMail',`password`='$pass',`phone`='$phone',`user_picture`='$picture',`gender`='$gender',`user_country`='$country',`user_fav_color`='$favColor' WHERE (`user_id`='$number');";
+                                $result = mysqli_query($connection, $query);
+                                if (!$result) {
+                                    die("DB update query failed.");
+                                }
+                                //                            sleep(3);
+                                echo 'UPDATE SUCCESFULL!';
+                                sleep(4);
+                                header('Location: ' . URL . 'userSettings.php');
+                            } else if (isset($_POST['is_remove']) && isset($_POST['del_user_id'])) {
+
+
+                                $user_id = $_POST["del_user_id"];
+                                if($user_id == 1){
+                                header('Location: ' . URL . 'admin.php');
+                                exit;
+
+                                }
+                                $query = "DELETE clth . * FROM tbl_222_clothes clth
+                                        INNER JOIN
+                                            tbl_222_closet_clothes clcs ON clth.clothing_id = clcs.clothing_id
+                                        INNER JOIN
+                                            tbl_222_closets clst ON clcs.closet_id = clst.closet_id 
+                                        WHERE
+                                            clst.user_id = $user_id;";
+                                $result = mysqli_query($connection, $query);
+                                if (!$result) {
+                                    die("DB delete clothing query failed.");
+                                }
+                                //delete the clothes
+                                $query = "DELETE clos . * FROM tbl_222_closets clos WHERE clos.user_id = $user_id;";
+                                $result = mysqli_query($connection, $query);
+                                if (!$result) {
+                                    die("DB delete closets query failed.");
+                                }
+                                //delete the closets
+                                $query = "DELETE usr . * FROM tbl_222_users usr WHERE usr.user_id = $user_id;";
+                                $result = mysqli_query($connection, $query);
+                                if (!$result) {
+                                    die("DB delete closets query failed.");
+                                }
+                                //delete user
+                                //send to login page
+                                
+                                header('Location: ' . URL . 'admin.php');
+                            } else if (isset($_POST['is_remove'])) {
+                                //first, get all clothes in closets that the user owns
+                                session_start();
+                                $number = $_SESSION['user_id'];
+                                $query = "DELETE clth . * FROM tbl_222_clothes clth
                                         INNER JOIN
                                             tbl_222_closet_clothes clcs ON clth.clothing_id = clcs.clothing_id
                                         INNER JOIN
                                             tbl_222_closets clst ON clcs.closet_id = clst.closet_id 
                                         WHERE
                                             clst.user_id = $number;";
-                            $result = mysqli_query($connection, $query);
-                            if (!$result) {
-                                die("DB delete clothing query failed.");
-                            }
-                            //delete the clothes
-                            $query = "DELETE clos . * FROM tbl_222_closets clos WHERE clos.user_id = $number;";
-                            $result = mysqli_query($connection, $query);
-                            if (!$result) {
-                                die("DB delete closets query failed.");
-                            }
-                            //delete the closets
-                            $query = "DELETE usr . * FROM tbl_222_users usr WHERE usr.user_id = $number;";
-                            $result = mysqli_query($connection, $query);
-                            if (!$result) {
-                                die("DB delete closets query failed.");
-                            }
-                            //delete user
-                            //send to login page
-                            sleep(5);
-                            header('Location: ' . URL . 'login.php');
-                        } else {
-                            $query = "INSERT INTO tbl_222_users(f_name,l_name,email,password,phone,user_picture,gender,user_country,user_fav_color) VALUES ('$fName','$lName','$eMail','$pass','$phone','$picture','$gender','$country','$favColor')";
-                            $result = mysqli_query($connection, $query);
-                            if (!$result) {
-                                die("DB insert query failed.");
-                            }
-//                            sleep(10);
+                                $result = mysqli_query($connection, $query);
+                                if (!$result) {
+                                    die("DB delete clothing query failed.");
+                                }
+                                //delete the clothes
+                                $query = "DELETE clos . * FROM tbl_222_closets clos WHERE clos.user_id = $number;";
+                                $result = mysqli_query($connection, $query);
+                                if (!$result) {
+                                    die("DB delete closets query failed.");
+                                }
+                                //delete the closets
+                                $query = "DELETE usr . * FROM tbl_222_users usr WHERE usr.user_id = $number;";
+                                $result = mysqli_query($connection, $query);
+                                if (!$result) {
+                                    die("DB delete closets query failed.");
+                                }
+                                //delete user
+                                //send to login page
+                                
+                                header('Location: ' . URL . 'login.php');
+                            } else {
+                                $query = "INSERT INTO tbl_222_users(f_name,l_name,email,password,phone,user_picture,gender,user_country,user_fav_color) VALUES ('$fName','$lName','$eMail','$pass','$phone','$picture','$gender','$country','$favColor')";
+                                $result = mysqli_query($connection, $query);
+                                if (!$result) {
+                                    die("DB insert query failed.");
+                                }
+                                //                            sleep(10);
 //                            header('Location: ' . URL . 'closet.php?closet_id='.$closet);
-                        }
-                        ?>
-                        <h5>Log into Clother</h5>
-                        <div class="col-1"></div>
-                        <div class="col text-center">
+                            }
+                            ?>
+                            <h5>Log into Clother</h5>
+                            <div class="col-1"></div>
+                            <div class="col text-center">
 
-                            <div class="row justify-content-center py-3">
+                                <div class="row justify-content-center py-3">
+                                </div>
                             </div>
+                            <div class="col-1"></div>
                         </div>
-                        <div class="col-1"></div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</main>
+    </main>
 </body>
+
 </html>
 <?php
 mysqli_close($connection);
